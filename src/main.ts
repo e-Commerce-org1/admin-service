a9a8423e737477cf1d3629b8fa3a8f8094d2b763
Akshit191002 <akki191002@gmail.com>
2025-06-09 10:56:55 +0530

    Admin module

--- src/main.ts
+++ src/main.ts
@@ -18,23 +18,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // Create the regular HTTP application
   const app = await NestFactory.create(AppModule);
-
-  // // Create the gRPC microservice
-  // const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.GRPC,
-  //     options: {
-  //       package: 'useradmin',
-  //       protoPath: join(__dirname, 'user.proto'),
-  //       url: '0.0.0.0:50051',
-  //     },
-  //   },
-  // );
-
-  // // Start both servers
-  // await grpcApp.listen();
-  await app.listen(3000);
-  console.log(`Application is running on: HTTP 3000, gRPC 50051`);
+  await app.listen(4000);
+  console.log(`Application is running on: HTTP 4000, gRPC 50051`);
 }
 bootstrap();
